5356001ec4258e93a6b99592f84c3e0449845ee5
litain <litain23@gmail.com>
2020-07-20 14:59:14 +0900

    Add Simpleservice

---
+++ simpleservice/src/main/java/org/simpleservice/controller/IndexController.java
@@ -0,0 +1,14 @@
+package org.simpleservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/index")
+    public String index() {
+        return "Hello this is index page";
+    }
+
+}
---
+++ simpleservice/src/test/java/org/simpleservice/controller/IndexControllerTest.java
@@ -0,0 +1,35 @@
+package org.simpleservice.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.Assert.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class IndexControllerTest {
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    public void indexTest() throws Exception {
+        String uri = "/index";
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+                .accept(MediaType.APPLICATION_JSON_VALUE)).andReturn();
+
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        assertEquals(content, "Hello this is index page");
+    }
+}
